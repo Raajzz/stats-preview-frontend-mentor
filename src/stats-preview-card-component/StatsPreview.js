5ecda97c5a2ed33935ed1ef9892a81620cafe9be
Raajzz <raajkumar.s2020@vitstudent.ac.in>
2022-06-03 11:36:32 +0530

    Completed desktop version

--- src/stats-preview-card-component/StatsPreview.js
+++ src/stats-preview-card-component/StatsPreview.js
@@ -1,23 +1,69 @@
-import React from 'react'
-import Helmet from 'react-helmet'
-import "./StatsPreview.css"
+import React from "react";
+import Helmet from "react-helmet";
+import "./StatsPreview.css";
+import imageHeader from "./assets/image-header-desktop.jpg";
 
 const StatsPreview = () => {
-  return (
-    <>
-      <Helmet>
-        <style>
-          {
-            `
+	return (
+		<>
+			<Helmet>
+				<style>
+					{`
               body{
                 background-color: hsl(233, 47%, 7%);
+                color: white;
+                font-family: 'Lexend Deca',sans-serif;
               }
-            `
-          }
-        </style>
-      </Helmet>
-    </>
-  )
-}
+            `}
+				</style>
+			</Helmet>
+			<div className="card-container">
+				<main className="card">
+					<div className="text-section">
 
-export default StatsPreview
+						<div>
+							<h1>
+								Get <span className="insights">insights</span> that help your
+								business grow.
+							</h1>
+							<p>
+								Discover the benefits of data analytics and make better
+								decisions regarding revenue, customer experience, and overall
+								efficiency.
+							</p>
+						</div>
+
+						<div className="stats-section">
+
+							<div>
+								<div className="title">10k+</div>
+								<div className="sub-title">COMPANIES</div>
+							</div>
+              
+							<div>
+								<div className="title">314</div>
+								<div className="sub-title">TEMPLATES</div>
+							</div>
+              
+							<div>
+								<div className="title">12M+</div>
+								<div className="sub-title">QUERIES</div>
+							</div>
+
+						</div>
+
+					</div>
+					<div className="image-section">
+						<img
+							src={imageHeader}
+							alt="People discussing and working"
+							className="image-class"
+						/>
+					</div>
+				</main>
+			</div>
+		</>
+	);
+};
+
+export default StatsPreview;
